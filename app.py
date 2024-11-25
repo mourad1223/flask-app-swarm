850eda27cd5460181efc99853479fb37c956da34
Damien Pereira <94973526+hellodamien@users.noreply.github.com>
2024-11-25 17:04:26 +0100

    feat: update main route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')  
 def home():  
-    return "Hello, Docker Swarm!"  
+    return "Hello, Docker Swarm! (v2)"  
 
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5000)  
