7b3701fe40f04d413fcba378123aec2b91b8f1f5
Damien Pereira <94973526+hellodamien@users.noreply.github.com>
2024-11-26 08:49:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')  
 def home():  
-    return "Hello, Docker Swarm! (v2)"  
+    return "Hello, Docker Swarm! (v2)"
+
+@app.route("/pinggy")
+def pinggy():
+    return "Pinggy is an Ngrok alternative https://pinggy.io/"
 
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5000)  
